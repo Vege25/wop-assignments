e255874f4b2423ab62a943a79dea92ed36e48816
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-11-02 15:13:37 +0200

    1

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,20 +1,18 @@
 "use strict";
 const express = require("express");
-const { cat_list_get, cat_get } = require("../controllers/catController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const {
+  cat_list_get,
+  cat_get,
+  cat_post,
+} = require("../controllers/catController");
 const router = express.Router();
 
 router.get("/", cat_list_get);
 
 router.get("/:id", cat_get);
 
-router.post("/", (req, res) => {
-  res.send("From this endpoint you can add cats.");
-});
-router.put("/", (req, res) => {
-  res.send("From this endpoint you can edit cats.");
-});
-router.delete("/", (req, res) => {
-  res.send("From this endpoint you can delete cats.");
-});
+router.post("/", upload.single("cat"), cat_post);
 
 module.exports = router;
