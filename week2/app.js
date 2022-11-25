dc28d577290c2b572767ff3ceeeed196bc727674
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-11-25 11:13:16 +0200

    week5

--- week2/app.js
+++ week2/app.js
@@ -1,5 +1,6 @@
 "use strict";
 // Router ottaa pyynnön selaimelta lähettää sen controllerille joka hakee tiedon modelista, joka palauttaa controllerille arvon
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoute = require("./routes/authRoute");
@@ -9,6 +10,23 @@ const { httpError } = require("./utils/errors");
 const passport = require("./utils/pass");
 const app = express();
 const port = 3000;
+
+app.enable("trust proxy");
+
+app.use((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // if express app run under proxy with sub path URL
+    // e.g. http://www.myserver.com/app/
+    // then, in your .env, set PROXY_PASS=/app
+    // Adapt to your proxy settings!
+    const proxypath = process.env.PROXY_PASS || "";
+    // request was via http, so redirect to https
+    res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+  }
+});
 
 app.use(cors());
 app.use(express.json()); // for parsing application/json
